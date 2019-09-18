301301696d3e30c4494da9e47920a0625886a235
watkissp <42682932+watkissp@users.noreply.github.com>
2019-09-18 15:20:01 -0400

    Update Magpie.java

--- src/Magpie.java
+++ src/Magpie.java
@@ -73,7 +73,12 @@ public class Magpie {
         } // Responses which require transformations
         else if (findKeyword(statement, "I want to", 0) >= 0) {
             response = transformIWantToStatement(statement);
-        } else {
+        } 
+        else if (findKeyword(statement, "I want", 0) >= 0) {
+            response = transformIWantStatement(statement);
+        }
+            
+        else {
             // Look for a two word (you <something> me)
             // pattern
             int psn = findKeyword(statement, "you", 0);
@@ -202,13 +207,34 @@ public class Magpie {
             statement = statement.substring(0, statement
                     .length() - 1);
         }
-        if (findKeyword(statement, "i want to") >= 0) {
-            String response = transformIWantToStatement(statement);
-        }
-        } else if (findKeyword(statement, "i want") >= 0) {
-            String response = "why do you want that?";
-            
+        int psn = findKeyword(statement, "I want to", 0);
+        String restOfStatement = statement.substring(psn + 9).trim();
+        return "What would it mean to " + restOfStatement + "?";
     }
+
+
+
+    /**
+     * Take a statement with "I want <something>." and transform it into
+     * "What would it mean if you had <something>?"
+     *
+     * @param statement the user statement, assumed to contain "I want"
+     * @return the transformed statement
+     */
+    private String transformIWantStatement(String statement) {
+        //  Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement
+                .length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement
+                    .length() - 1);
+        }
+        int psn = findKeyword(statement, "I want", 0);
+        String restOfStatement = statement.substring(psn + 7).trim();
+        return "What would it mean if you had " + restOfStatement + "?";
+    }
+            
 
     /**
      * Take a statement with "you <something> me" and transform it into "What
